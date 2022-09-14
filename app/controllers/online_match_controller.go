f246a980ce54aa404c0dadc5d01aab8d71051ba5
kenboo0426 <kenshin.kenboo@gmail.com>
2022-09-14 12:38:43 +0900

    move file

--- app/controllers/online_match_controller.go
+++ app/controllers/online_match_controller.go
@@ -2,17 +2,13 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-	"sort"
 	"strconv"
 	"strings"
 	"winning_fish_backend/app/models"
-
-	"github.com/gorilla/websocket"
 )
 
 func HandleOnlineMatchRequest(w http.ResponseWriter, r *http.Request) {
@@ -45,124 +41,6 @@ func HandleOnlineMatchUpdateRequest(w http.ResponseWriter, r *http.Request) {
 	default:
 		w.WriteHeader(405)
 	}
-}
-
-var upgradeConnection = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
-
-var (
-	wsChan  = make(chan models.WsPayload)
-	clients = make(map[models.WebSocketConnection]string)
-)
-
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgradeConnection.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println("ok client connectiong")
-
-	var response models.WsJsonResponse
-
-	conn := models.WebSocketConnection{Conn: ws}
-	clients[conn] = ""
-
-	err = ws.WriteJSON(response)
-	if err != nil {
-		log.Println(err)
-	}
-
-	go ListenFowWs(&conn)
-}
-
-func ListenFowWs(conn *models.WebSocketConnection) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("Error", fmt.Sprintf("%v", r))
-		}
-	}()
-
-	var payload models.WsPayload
-
-	for {
-		err := conn.ReadJSON(&payload)
-		fmt.Println(payload, "payload")
-
-		if err != nil {
-			log.Println(err)
-		} else {
-			payload.Conn = *conn
-			wsChan <- payload
-		}
-	}
-}
-
-func broadcastToAll(response models.WsJsonResponse) {
-	for client := range clients {
-		err := client.WriteJSON(response)
-		if err != nil {
-			log.Println(err)
-			_ = client.Close()
-			delete(clients, client)
-		}
-	}
-}
-
-func ListenToWsChannel() {
-	var response models.WsJsonResponse
-
-	for {
-		e := <-wsChan
-
-		switch e.Action {
-		case "fetch_joined_user":
-			user_ids := getUserList()
-			response.Action = "list_users"
-			response.JoinedOnlineMatchUserIDs = user_ids
-		case "join_online_match":
-			var userID string
-			if e.UserID == "" {
-				// var user models.User
-				// user.UUID = models.CreateUUID().String()
-				// user.Name = "guests"
-				// user.Role = 2
-				// user.CreateUser()
-				// userID = string(user.ID)
-			} else {
-				userID = string(e.UserID)
-			}
-
-			if !include(clients, userID) {
-				clients[e.Conn] = userID
-			}
-			user_ids := getUserList()
-			response.Action = "list_users"
-			response.JoinedOnlineMatchUserIDs = user_ids
-		case "left":
-			delete(clients, e.Conn)
-			user_ids := getUserList()
-			response.Action = "list_users"
-			response.JoinedOnlineMatchUserIDs = user_ids
-		}
-		fmt.Println(response, "resposne")
-
-		broadcastToAll(response)
-	}
-}
-
-func getUserList() []string {
-	var clientList []string
-	for _, client := range clients {
-		if client != "" {
-			clientList = append(clientList, client)
-		}
-	}
-
-	sort.Strings(clientList)
-	return clientList
 }
 
 func indexOnlineMatch(w http.ResponseWriter, r *http.Request) {
@@ -231,27 +109,4 @@ func startOnlineMatch(w http.ResponseWriter, r *http.Request) {
 
 	res, _ := json.Marshal(online_match)
 	w.Write(res)
-}
-
-func uniq(target []string) (result []string) {
-	m := map[string]bool{}
-
-	for _, v := range target {
-		if !m[v] {
-			m[v] = true
-			result = append(result, v)
-		}
-	}
-
-	return result
-}
-
-func include(array map[models.WebSocketConnection]string, target string) bool {
-	for _, item := range array {
-		if item == target {
-			return true
-		}
-	}
-
-	return false
 }
---
+++ app/controllers/websocket_controller.go
@@ -0,0 +1,154 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"winning_fish_backend/app/models"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+var (
+	wsChan  = make(chan models.WsPayload)
+	clients = make(map[models.WebSocketConnection]models.WsUser)
+)
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("ok client connectiong")
+
+	var response models.WsJsonResponse
+
+	conn := models.WebSocketConnection{Conn: ws}
+	clients[conn] = models.WsUser{}
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		log.Println(err)
+	}
+
+	go ListenFowWs(&conn)
+}
+
+func ListenFowWs(conn *models.WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Error", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	var payload models.WsPayload
+
+	for {
+		err := conn.ReadJSON(&payload)
+		fmt.Println(payload, "payload")
+
+		if err != nil {
+			log.Println(err)
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func broadcastToAll(response models.WsJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println(err)
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+func ListenToWsChannel() {
+	var response models.WsJsonResponse
+
+	for {
+		e := <-wsChan
+
+		switch e.Action {
+		case "fetch_joined_user":
+			users := getUserList()
+			response.Action = "list_users"
+			response.Users = users
+
+		case "join_online_match":
+			var userID string
+			if e.UserID == "" {
+				// var user models.User
+				// user.UUID = models.CreateUUID().String()
+				// user.Name = "guests"
+				// user.Role = 2
+				// user.CreateUser()
+				// userID = string(user.ID)
+			} else {
+				userID = string(e.UserID)
+			}
+
+			if !include(clients, userID) {
+
+				clients[e.Conn] = models.WsUser{ID: userID, Name: e.UserName}
+			}
+			users := getUserList()
+			response.Action = "list_users"
+			response.Users = users
+		case "left":
+			delete(clients, e.Conn)
+			user_ids := getUserList()
+			response.Action = "list_users"
+			response.Users = user_ids
+		}
+		fmt.Println(e.UserName, "name")
+		fmt.Println(response, "resposne")
+
+		broadcastToAll(response)
+	}
+}
+
+func getUserList() []models.WsUser {
+	var clientList []models.WsUser
+	for _, client := range clients {
+		fmt.Printf("p :%+v\n", client)
+		if client.ID != "" {
+			clientList = append(clientList, client)
+		}
+	}
+
+	return clientList
+}
+
+func uniq(target []string) (result []string) {
+	m := map[string]bool{}
+
+	for _, v := range target {
+		if !m[v] {
+			m[v] = true
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
+
+func include(array map[models.WebSocketConnection]models.WsUser, target string) bool {
+	for _, item := range array {
+		if item.ID == target {
+			return true
+		}
+	}
+
+	return false
+}
