8698fb4a176093ee49fe27c3630b442b89de8c0d
kenboo0426 <kenshin.kenboo@gmail.com>
2022-09-14 12:38:16 +0900

    create wsuser

--- app/models/websocket.go
+++ app/models/websocket.go
@@ -4,10 +4,16 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type WsUser struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 type WsJsonResponse struct {
-	Action                   string   `json:"action"`
-	Message                  string   `json:"message"`
-	JoinedOnlineMatchUserIDs []string `json:"joined_onine_match_user_ids"`
+	Action                   string      `json:"action"`
+	JoinedOnlineMatchUserIDs []string    `json:"joined_onine_match_user_ids"`
+	Users                    []WsUser    `json:"users"`
+	OnlineMatch              OnlineMatch `json:"online_match"`
 }
 
 type WebSocketConnection struct {
@@ -15,8 +21,8 @@ type WebSocketConnection struct {
 }
 
 type WsPayload struct {
-	Action  string              `json:"action"`
-	Message string              `json:"message"`
-	UserID  string              `json:"user_id"`
-	Conn    WebSocketConnection `json:"-"`
+	Action   string              `json:"action"`
+	UserID   string              `json:"user_id"`
+	UserName string              `json:"user_name"`
+	Conn     WebSocketConnection `json:"-"`
 }
