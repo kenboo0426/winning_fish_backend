a29e61c2cf17c7d8b9dbe1d6b75eb42b3facb1d2
kenboo0426 <kenshin.kenboo@gmail.com>
2022-10-16 23:28:07 +0900

    change struct and name

--- app/models/websocket.go
+++ app/models/websocket.go
@@ -12,21 +12,27 @@ type WsUser struct {
 }
 
 type WsResponse struct {
-	Action                   string      `json:"action"`
-	JoinedOnlineMatchUserIDs []string    `json:"joined_onine_match_user_ids"`
-	Users                    []WsUser    `json:"users"`
-	OnlineMatch              OnlineMatch `json:"online_match"`
+	Action                   string   `json:"action"`
+	// JoinedOnlineMatchUserIDs []string `json:"joined_onine_match_user_ids"`
+	Users                    []WsUser `json:"users"`
+	OnlineMatchID            int      `json:"online_match_id"`
 }
 
-type WebSocketConnection struct {
+type WsConnection struct {
 	*websocket.Conn
 }
 
 type WsRequest struct {
-	Action       string              `json:"action"`
-	UserID       string              `json:"user_id"`
-	UserName     string              `json:"user_name"`
-	RemainedTime float32             `json:"remained_time"`
-	UserIcon     string              `json:"user_icon"`
-	Conn         WebSocketConnection `json:"-"`
+	Action        string       `json:"action"`
+	UserID        string       `json:"user_id"`
+	UserName      string       `json:"user_name"`
+	RemainedTime  float32      `json:"remained_time"`
+	UserIcon      string       `json:"user_icon"`
+	OnlineMatchID int          `json:"online_match_id"`
+	Conn          WsConnection `json:"-"`
+}
+
+type WsClient struct {
+	OnlineMatchID int
+	WsUser        WsUser
 }
